1cd3c81ddf582e61f2cdd4c9dcccc195f1ec4028
Luana-s <luana.silva1@arapiraca.ufal.br>
2022-10-10 17:12:33 -0300

    Exericio5 Template Method

--- exercicio5/Main.java
+++ exercicio5/Main.java
@@ -3,6 +3,7 @@ package exercicio5;
 public class Main {
     public static void main(String[]args){
         UFALBluetooth serial = new Serial();
+        System.out.println("\n");
         UFALBluetooth baseadoEmPacotes =new BaseadoEmPacotes();
         
 
