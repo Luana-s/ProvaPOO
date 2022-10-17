5364bceb0cd1a6c5edfb6f6a52b538e5f8bf6e7e
Luana-s <luana.silva1@arapiraca.ufal.br>
2022-10-17 00:59:16 -0300

    Exercicio6

--- exercicio6/Cliente.java
+++ exercicio6/Cliente.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class Cliente {
     protected String nome;
     protected int agencia;
+    private ArrayList<ContaCorrente> contaCorrente = new ArrayList<>();
+
+    public ArrayList<ContaCorrente> getContaCorrente() {
+        return contaCorrente;
+    }
+
+    public void setContaCorrente(ArrayList<ContaCorrente> contaCorrente) {
+        this.contaCorrente = contaCorrente;
+    }
 
     public Cliente(String string) {
     }
@@ -27,5 +38,11 @@ public class Cliente {
     }
 
     public void cliente(String string) {
+    }
+
+    public void quantContaCorrente(){
+        for (ContaCorrente conta :  contaCorrente) {
+            System.out.println(conta);
+        }
     }
 }
